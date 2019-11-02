9fac2fa2f77935e553f618612eb9f23305091410
Tiago Lima <tiagolima@Tiagos-Mac-mini.local>
2019-11-01 23:34:14 -0300

    Created the Backend with NodeJS.

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-8d1jw.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// req.query = Acessar query params (para filtros).
+// req.params = Acessar route params (para edição e delete).
+// req.body = Acessar corpo da requisição ( para criação e edição).
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
